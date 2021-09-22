e1201b8ef5215b5489c75e44f61908f47eada796
Bohdan Mateiko <bmatei@softserveinc.com>
2021-09-22 16:47:06 +0300

    fixes deprecated datasource parameter name in testinfra module

--- testinfra/main.tf
+++ testinfra/main.tf
@@ -2,6 +2,6 @@ provider "aws" {}
 
 data "aws_availability_zones" "available" {
   # c4.large instance type not available in these AZ
-  blacklisted_zone_ids = ["usw2-az4"]
-  state                = "available"
+  exclude_names = ["usw2-az4"]
+  state         = "available"
 }
